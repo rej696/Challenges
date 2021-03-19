31745849ccd4f45409cb26229fc75a9d454c80f6
Rowan_Surface <55083730+rej696@users.noreply.github.com>
2021-03-19 09:55:45 +0000

    update to include part 2 of rednoseday 2021 challenge

--- Rowan/altran-rednoseday-2021-haskell/main.hs
+++ Rowan/altran-rednoseday-2021-haskell/main.hs
@@ -3,45 +3,50 @@ keypad :: [[Int]]
 keypad = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 
 -- move function for right and down increments
-moveRD coord =
+moveRD :: Int -> Bool -> Int
+moveRD coord partflag =
     if coord == 2
-        then coord
+        then if partflag then coord - 1 else coord 
     else coord + 1
 
 -- move function for left and up increments
-moveLU coord =
+moveLU :: Int -> Bool -> Int
+moveLU coord partflag =
     if coord == 0
-        then coord
+        then if partflag then coord + 1 else coord
     else coord - 1
 
 -- Determines the final coordinate from a line
 -- and start coordinate recursively
-move :: [Int] -> String -> [Int]
-move coord line =
+move :: [Int] -> String -> Bool -> [Int]
+move coord line partflag =
     if line /= [] 
         then
             let command = head line
                 rest = tail line
             in move (case command of
-                'U' -> [head coord, moveLU (last coord)]
-                'D' -> [head coord, moveRD (last coord)]
-                'L' -> [moveLU (head coord), last coord]
-                'R' -> [moveRD (head coord), last coord]
-            ) rest
+                'U' -> [head coord, moveLU (last coord) partflag]
+                'D' -> [head coord, moveRD (last coord) partflag]
+                'L' -> [moveLU (head coord) partflag, last coord]
+                'R' -> [moveRD (head coord) partflag, last coord]
+            ) rest partflag
     else coord
 
 -- Builds output from start coordinate and list of strings (lines)
-getResult :: [Int] -> [[Char]] -> [Int]
-getResult startcoord input =
-    let newcoord = move startcoord (head input)
+getResult :: [Int] -> [[Char]] -> Bool -> [Int]
+getResult startcoord input partflag =
+    let newcoord = move startcoord (head input) partflag
         number = (keypad !! last newcoord) !! head newcoord
         rest =
             if (tail input) == [] 
                 then [] 
-             else getResult newcoord (tail input)
+             else getResult newcoord (tail input) partflag
     in number : rest
 
--- read standard input and convert to list of strings, output result
+-- read standard input and convert to list of strings, output result for each part
 main = do
     input <- getContents
-    print (getResult [1, 1] (lines input))
+    putStr "Part1: "
+    print (getResult [1, 1] (lines input) False)
+    putStr "Part2: "
+    print (getResult [1, 1] (lines input) True)
